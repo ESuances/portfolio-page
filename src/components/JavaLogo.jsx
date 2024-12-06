6dd3e78725f2731c0d4637ffdb8fb1dfaf5a2610
ESuances <erick.s.suances.arc@gmail.com>
2024-12-05 21:17:18 -0600

    First 3D models!
    Mis primeros modelos 3D ahora si en mi portafolio, no solo utilice el del video, tambien cree mi propio logo de Java y lo improte en la escena, haciendo una muy cool aparicion en la posicion exacta, logre poner mi propio logo en escena!!!

---
+++ src/components/JavaLogo.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import gsap from 'gsap'
+import {useGSAP} from "@gsap/react";
+
+const JavaLogo = (props) => {
+    const JavaLogoRef = useRef()
+    const { nodes, materials } = useGLTF('/models/Java_Logo.glb')
+
+    //Animation to make 3d object jump in space
+    useGSAP(() => {
+       gsap.to(JavaLogoRef.current.position, {
+           y: JavaLogoRef.current.position.y + 0.5,
+           duration: 1.5,
+           repeat: -1,
+           yoyo: true,
+       })
+    });
+
+    return (
+        <group {...props} ref={JavaLogoRef} rotation={[0, -19.5, 0]}>
+            <group position={[0, -0.479, -0.085]} scale={0.585}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Torus004.geometry}
+                    material={materials.Java_Cup}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Torus004_1.geometry}
+                    material={materials.Java_Smoke}
+                />
+            </group>
+        </group>
+    )
+}
+
+useGLTF.preload('/models/Java_Logo.glb')
+export default JavaLogo
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,95 @@
+// noinspection LanguageDetectionInspection
+
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+//import {Leva, useControls} from "leva"
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import JavaLogo from "../components/JavaLogo.jsx";
 
 const Hero = () => {
+    /*
+    const controls = useControls('HackerRoom', {
+        positionX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        scale: {
+            value: 1,
+            min: 0.1,
+            max: 10,
+        }
+    })
+
+     */
+    const isSmall = useMediaQuery({maxWidth: 440});
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
     return (
-        <div>Hero</div>
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi, I'm Erick Suances / ESuances <span className="waving-hand">👋️</span></p>
+                <p className="hero_tag text-gray_gradient">Bulding games, apps and Saas</p>
+            </div>
+            <div className="w-full h-full absolute inset-0">
+                {/*<Leva />*/}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0,0,20]} />
+                        <HackerRoom
+                            /*
+                            scale={isMobile ? 0.07 : 0.1}
+                            position={[1.1,-10.0,-10.0]}
+                            rotation={[0.1,-3.1,0.0]}
+                            */
+                            scale={sizes.deskScale}
+                            position={sizes.deskPosition}
+                            rotation={[0.1,-3.1,0.0]}
+                            //Controls to properly setup a 3D object on the scene
+                            //scale={[controls.scale, controls.scale, controls.scale]}
+                            //position={[controls.positionX,controls.positionY,controls.positionZ]}
+                            //rotation={[controls.rotationX,controls.rotationY,controls.rotationZ]}
+                        />
+                        <group>
+                            <JavaLogo position={sizes.JavaLogoPosition}/>
+                        </group>
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[10,10,10]} intensity={1}/>
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
     )
 }
 export default Hero
